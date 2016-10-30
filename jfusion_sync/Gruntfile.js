fa4aa1df7046034c8148dbb3e5b54d2f527ea1de
Mark Gawler <markg@wler.co.uk>
2016-10-30 19:56:57 +0000

    update to work from environment vars

--- jfusion_sync/Gruntfile.js
+++ jfusion_sync/Gruntfile.js
@@ -2,19 +2,23 @@ module.exports = function(grunt) {
   require('jit-grunt')(grunt);
 
   grunt.initConfig({
+	RemoteHost: process.env.ukrgbRemoteHost,
+	RemoteUser: process.env.ukrgbRemoteUser,
+	privateKeyFile: process.env.ukrgbPrivateKeyFile,
        
     synchard: {
         remotedest: {
             options: {
             	args: ['-av','--delete'],
-            	exclude: ['language','css'],
+            	exclude: ['css'],
                 ssh: true,
-                privateKey: "/home/mrfg/.ssh/Area51.pem"
+                privateKey: "<%=privateKeyFile%>"
+
             },
             files: {
-            	'ubuntu@area51.ukriversguidebook.co.uk:/var/www/ukrgb/phpbb/ext/jfusion': ['components/com_jfusion/plugins/phpbb31/jfusion/'],
-            	'ubuntu@area51.ukriversguidebook.co.uk:/var/www/ukrgb/joomla/components/com_jfusion/': ['components/com_jfusion/'],
-            	'ubuntu@area51.ukriversguidebook.co.uk:/var/www/ukrgb/joomla/administrator/components/com_jfusion/': ['administrator/components/com_jfusion/'],                
+            	'<%=RemoteUser%>@<%=RemoteHost%>:/var/www/ukrgb/phpbb/ext/jfusion': ['components/com_jfusion/plugins/phpbb31/jfusion/'],
+//            	'<%=RemoteUser%>@<%=RemoteHost%>:/var/www/ukrgb/joomla/components/com_jfusion/': ['components/com_jfusion/'],
+//            	'<%=RemoteUser%>@<%=RemoteHost%>:/var/www/ukrgb/joomla/administrator/components/com_jfusion/': ['administrator/components/com_jfusion/'],                
             }
         }
     },
@@ -29,7 +33,7 @@ module.exports = function(grunt) {
       }
     }
   });
-  
+ 
   grunt.file.setBase('../../org.jfusion.jfusion/')
   
   grunt.registerTask('sync', ['synchard']);
