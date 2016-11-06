9e007e919449c36fd1f69e403c28409a974e857c
Mark Gawler <markg@wler.co.uk>
2016-11-06 16:53:43 +0000

    Add PayPal button

--- ext/ukrgb/template/event/main_listener.php
+++ ext/ukrgb/template/event/main_listener.php
@@ -23,7 +23,7 @@ class main_listener implements EventSubscriberInterface
 	{
 		return array(
 				'core.user_setup' => 'core_user_setup',
-				
+				'core.viewforum_get_topic_data' => 'core_viewforum_get_topic_data',
 		);
 	}
 
@@ -36,19 +36,27 @@ class main_listener implements EventSubscriberInterface
 	/** @var \phpbb\config\config */
 	protected $config;
 	
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/** @var \PDO */
+	protected $pdo;
+	
 	/**
 	 * Constructor
 	 *
 	 * @param \phpbb\template\template             $template          Template object
 	 * @param \phpbb\cache\driver\driver_interface $cache             Cache driver interface
-	 * @param \phpbb\config\config                 $config			
+	 * @param \phpbb\config\config                 $config	
 
 	 */
-	public function __construct(\phpbb\template\template $template, \phpbb\cache\driver\driver_interface $cache, \phpbb\config\config $config)
+	public function __construct(\phpbb\template\template $template, 
+			\phpbb\cache\driver\driver_interface $cache, \phpbb\config\config $config)
 	{
 		$this->template = $template;
 		$this->cache = $cache;
 		$this->config = $config;
+		$this->pdo = null;
 	}
 	
 	/**
@@ -64,6 +72,45 @@ class main_listener implements EventSubscriberInterface
 			$this->load_banner($event);
 		}
 	}
+	
+	/**
+	 * core viewforum get topic data Event Handler
+	 * 
+	 *  @param unknown $event
+	 */
+	public function core_viewforum_get_topic_data($event){
+		// Get the Jommla com_ukrgb component parameters
+		$params = $this->load_component_params();
+		
+		$forum_id = $event['forum_id'];
+		if ($forum_id == 12)
+		{
+			// Set up PayPal button parameters
+			if ($params->sandbox)
+			{
+				$this->template->assign_vars(array(
+						'UKRGB_SHOW_PP_DONATE' => true,
+						'U_UKRGB_PP_URL' => "https://www.sandbox.paypal.com/cgi-bin/webscr",
+						'UKRGB_PP_HOSTED_BUTTON_ID' => $params->sandbox_hosted_button_id,
+				));
+			}
+			else
+			{
+				$this->template->assign_vars(array(
+						'UKRGB_SHOW_PP_DONATE' => true,
+						'U_UKRGB_PP_URL' => "https://www.paypal.com/cgi-bin/webscr",
+						'UKRGB_PP_HOSTED_BUTTON_ID' => $params->hosted_button_id,
+				));
+			}
+		}
+		else 
+		{
+			$this->template->assign_vars(array(
+					'UKRGB_SHOW_PP_DONATE' => false,
+			));
+		}
+	}
+	
 	
 	/**
 	 * Language setup
@@ -93,24 +140,9 @@ class main_listener implements EventSubscriberInterface
 		$banner_data = $this->cache->get('_ukrgb_banner_data');
 		if ($banner_data == false)
 		{
-			// get banner data from Joomla database an cache the html.
+			// get banner data from Joomla database and cache the html.
 			
-			$host = $this->config['ukrgb_jdbhost'];
-			$db  =  $this->config['ukrgb_jdb'];
-			$user = $this->config['ukrgb_jdbuser'];
-			$pass = $this->config['ukrgb_jdbpwd'];
-			$charset = 'utf8';
-				
-			
-			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-			
-			$opt = [
-					\PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-					\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-					\PDO::ATTR_EMULATE_PREPARES   => false,
-			];
-			
-			$pdo = new \PDO($dsn, $user, $pass, $opt);
+			$pdo = $this->get_joomla_db();
 			
 			$stmt = $pdo->query('SELECT `name`,`clickurl`,`params`,`custombannercode` FROM `jos_banners` WHERE `state`=1');
 
@@ -145,6 +177,65 @@ class main_listener implements EventSubscriberInterface
 				'U_UKRGB_BANNER_LEFT' => $html['l'],
 				'U_UKRGB_BANNER_RIGHT' => $html['r'],
 		));
+		
+	}
+	
+	/**
+	 * Get the Jommla com_ukrgb component parameters
+	 * 
+	 * return $params 
+	 */
+	private function load_component_params()
+	{
+		$params = $this->cache->get('_ukrgb_com_params');
+		if ($params == false)
+		{
+			$pdo = $this->get_joomla_db();
+				
+			$stmt = $pdo->query("SELECT `params` FROM `jos_extensions` WHERE `name` = 'com_ukrgb'");
+			$row = $stmt->fetch();
+			$params = json_decode($row['params']);
+			
+			$this->cache->put('_ukrgb_com_params',$params);
+		}
+		return $params;		
+	}
+	
+	
+	/**
+	 * Create Joomla DB connection object if the object dose not exist.
+	 * Return a PDO object.
+	 * 
+	 * @return \PDO|PDO
+	 */
+	private function get_joomla_db()
+	{
+		if ($this->pdo == null)
+		{				
+			$host = $this->config['ukrgb_jdbhost'];
+			$db  =  $this->config['ukrgb_jdb'];
+			$user = $this->config['ukrgb_jdbuser'];
+			$pass = $this->config['ukrgb_jdbpwd'];
+			$charset = 'utf8';
+			
+				
+			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+				
+			$opt = [
+					\PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+					\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+					\PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+				
+			$pdo = new \PDO($dsn, $user, $pass, $opt);
+			$this->pdo = $pdo;
+		}
+		else
+		{
+			$pdo = $this->pdo;
+		}
+		
+		return $pdo;
 		
 	}
 	
